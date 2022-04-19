501b889388b2dda6e44a2dda960a8ca27604f9b0
Julia Ivanova <for.julia96@gmail.com>
2022-04-19 18:40:54 +0300

    Setup first screen

--- app/src/main/java/com/example/android_quiz/MainActivity.kt
+++ app/src/main/java/com/example/android_quiz/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.android_quiz
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT < 16) {
+            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        } else {
+            val controller = window.insetsController
+            controller?.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            controller?.hide(WindowInsets.Type.statusBars())
+        }
     }
 }
