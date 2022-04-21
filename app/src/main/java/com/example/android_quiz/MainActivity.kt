56684d479bf11438b263ba2c3e22b17087147f36
Julia Ivanova <for.julia96@gmail.com>
2022-04-21 20:22:11 +0300

    Add second activity

--- app/src/main/java/com/example/android_quiz/MainActivity.kt
+++ app/src/main/java/com/example/android_quiz/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.android_quiz
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowInsetsController
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.R)
@@ -21,6 +24,17 @@ class MainActivity : AppCompatActivity() {
             val controller = window.insetsController
             controller?.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
             controller?.hide(WindowInsets.Type.statusBars())
+        }
+
+        btn_start.setOnClickListener {
+            if (et_name.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val intent = Intent(this, QuizQuestionsActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
